3611afd5ec983457ff5b5b38cd233d142fe857e8
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2023-09-04 17:00:24 -0400

    feat: primeiras requisições

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log('sucess!');
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
+
+fetch(url)
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(err) {
+        console.error(err);
+    })
